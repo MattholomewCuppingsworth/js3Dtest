580672934d4508a7f521167cc03dc7127b5d4c7e
MPeeples <mpeeples@sagecloud.com>
2015-02-27 16:35:56 -0500

    Added texture files, started writing texture loader

--- start.js
+++ start.js
@@ -116,6 +116,22 @@ var initBuffers = function() {
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(generatedColors), gl.STATIC_DRAW);
 }
 
+var initTextures = function() {
+	cubeTexture = gl.createTexture();
+	cubeImage = new Image();
+	cubeImage.onload = function() { handleTextureLoaded(cubeImage, cubeTexture); }
+	cubeImage.src = "tex/onePip.png";
+}
+
+var handleTextureLoaded = function(image, texture) {
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+	gl.generateMipMap(gl.TEXTURE_2D);
+	gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
 var drawScene = function() {
 	// Clear the canvas every redraw
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
